6b4e7f9a9826edc23a6dfd3cd777049ca9a324bd
Krek <77121649+kraksy@users.noreply.github.com>
2022-11-03 18:26:37 +0100

    add

--- main.py
+++ main.py
@@ -2,24 +2,23 @@ import pygame as py
 import sys as sus
 from sett import *
 
-
 class gaem:
 
     screen = py.display.set_mode((WIDTH, HEIGHT))
 
     def draw_grid():
         for x in range(0, WIDTH, TILESIZE):
-            py.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+            py.draw.line(py.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
         for y in range(0, HEIGHT, TILESIZE):
-            py.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+            py.draw.line(py.screen, LIGHTGREY, (0, y), (WIDTH, y))
 
     def event():
         for event in py.event.get():
             if event.type == py.QUIT:
-                self.quit()
+                py.quit()
             if event.type == py.KEYDOWN:
                 if event.key == py.K_ESCAPE:
-                    self.quit()
+                    py.quit()
 
     while running:
         event()
--- wall.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="wall" tilewidth="16" tileheight="16" tilecount="1" columns="1">
- <image source="../../Downloads/pixilart-drawing.png" trans="ffffff" width="16" height="16"/>
-</tileset>
