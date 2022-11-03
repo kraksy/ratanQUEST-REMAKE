3ce1d2b665e1b636ebbdd34d75c7908f704b58c8
Krek <77121649+kraksy@users.noreply.github.com>
2022-11-03 18:27:59 +0100

    change

--- main.py
+++ main.py
@@ -6,12 +6,6 @@ class gaem:
 
     screen = py.display.set_mode((WIDTH, HEIGHT))
 
-    def draw_grid():
-        for x in range(0, WIDTH, TILESIZE):
-            py.draw.line(py.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
-        for y in range(0, HEIGHT, TILESIZE):
-            py.draw.line(py.screen, LIGHTGREY, (0, y), (WIDTH, y))
-
     def event():
         for event in py.event.get():
             if event.type == py.QUIT:
